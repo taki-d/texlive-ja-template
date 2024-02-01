206430699758ed71253b1abdf05e7f1d36f7612c
taki-d <mail@orangepla.net>
2024-02-01 17:57:22 +0900

    add figure

---
+++ figure/pull_graphs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sftp pegasus:/work/NBB/nosita/hdf5-vfd-chfs/analyze/*.eps .
+sftp pegasus:/work/NBB/nosita/pmem_bench/jupyter/*.eps .
+
+# dirs=`find . -maxdepth 0 -type f -name *.eps`
+
